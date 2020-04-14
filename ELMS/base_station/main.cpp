6fa69d3b110bc2fd7af115bc7f4431e8cfdcff2e
kretzscd <kretzscd@oregonstate.edu>
2020-04-14 11:12:58 -0600

    Added sample code to find COM port
    This was a test script from the stack overflow article on how to identify the COM port being used.

--- ELMS/base_station/main.cpp
+++ ELMS/base_station/main.cpp
@@ -2,8 +2,44 @@
 * ELMS - Trevor Frame, Andrew Freitas, Deborah Kretzschmar
 */
 
+#include <iostream>
+#include <string>
+#include <Windows.h>
+using std::string;
+
+/* Reference for this code is stack overflow
+ * https://stackoverflow.com/questions/2674048/what-is-proper-way-to-detect-all-available-serial-ports-on-windows/
+ */
+
+bool SelectComPort() //added function to find the present serial 
+{
+    char lpTargetPath[5000]; // buffer to store the path of the COMPORTS
+    bool gotPort = false; // in case the port is not found
+
+    for (int i = 0; i < 255; i++) // checking ports from COM0 to COM255
+    {
+        std::string str = "COM" + std::to_string(i); // converting to COM0, COM1, COM2
+        DWORD test = QueryDosDevice(str.c_str(), lpTargetPath, 5000);
+
+        // Test the return value and error if any
+        if (test != 0) //QueryDosDevice returns zero if it didn't find an object
+        {
+            std::cout << str << ": " << lpTargetPath << std::endl;
+            gotPort = true;
+        }
+
+        if (::GetLastError() == ERROR_INSUFFICIENT_BUFFER)
+        {
+        }
+    }
+
+    return gotPort;
+}
 
 int main()
 {
-	return 0;
+    bool found = SelectComPort();
+
+
+    return 0;
 }
