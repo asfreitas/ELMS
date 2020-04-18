9cef0504e4dec81c481f3c06279ee0315dccb526
kretzscd <kretzscd@oregonstate.edu>
2020-04-18 17:45:19 -0600

    part of Base_Unit class written with its members and functions.

--- ELMS/base_station/base_unit.h
+++ ELMS/base_station/base_unit.h
@@ -1,3 +1,57 @@
 /*
 * ELMS - Trevor Frame, Andrew Freitas, Deborah Kretzschmar
 */
+#ifndef BASE_UNIT_H
+#define BASE_UNIT_H
+
+
+#include <iostream>
+#include <fstream>
+#include <istream>
+#include <string>
+#include <Windows.h>
+using std::string;
+
+//this is how many messages will be in each log file before it is close. 
+// this is easily changed and we can make any number we want. 
+#define MESSAGE_LIMIT 5
+
+class Base_Unit
+{
+    // using a static variable will be the same value for all class objects. 
+    static int messageCount;
+    static int alertCount;
+    static int networkCount;
+    static int miscCount;
+    static string logFileName;
+    static string alertFile;
+    static string netFailFile;
+    static string miscErrorFile;
+
+    //this is the path to the folder that stores the logs. It has a set function
+    // to allow the user to create the folder wherever they want.  
+    string pathToLogs = "C:\\logs";
+public:
+    bool checkMessageCount(int type);
+    bool logFile(std::ofstream &logFile, string* inputMessage, int type);
+    void lockCloseFile(std::ofstream& logFile, int type);
+    void lockWriteFile(std::ofstream& logFile, string* inputMessage);
+    int getMessageCount(int type);
+    void resetMessageCount(int type);
+    void incMessageCount(int type);
+    void createFileName(string* fileName, int type);
+    void createFolder(string& pathToCreate);
+    bool directoryExists(const std::string& directoryName);
+    void setPathToLogs(string& path);
+    string getPathToLogs();
+    string getPathToMessages();
+    string getPathToAlerts();
+    string getPathToNetWorkFailure();
+    string getPathToMiscErrors();
+    string getCurrentFileName(int type);
+    void setFileName(int type);
+
+
+};
+
+#endif // !BASE_UNIT_H
