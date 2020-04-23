6037015947c2a0daea3d1e2916001b1f920dba59
kretzscd <kretzscd@oregonstate.edu>
2020-04-23 11:15:19 -0600

    Added new file outgoing.h
    Contains function declarations

---
+++ ELMS/base_station/outgoing.h
@@ -0,0 +1,9 @@
+#ifndef OUTGOING_H
+#define OUTGOING_H
+#include <string>
+using std::string;
+
+void outgoing_message(string& filePath, int unit1, int unit2, int messageTime, int velocity, int distance, int bearing);
+void network_failure_message(string& networkFailureMsg);
+void misc_error_message(string& miscErrorMsg);
+#endif
