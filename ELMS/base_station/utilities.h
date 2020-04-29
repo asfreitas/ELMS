9df613d0adba6253caf94e046edf4659bfb2a91b
kretzscd <kretzscd@oregonstate.edu>
2020-04-29 16:34:10 -0600

    Added function declaration to change a double to a string

--- ELMS/base_station/utilities.h
+++ ELMS/base_station/utilities.h
@@ -4,12 +4,15 @@
 #include <iostream>
 #include "vehicle.h"
 #include <algorithm>
+#include <sstream>
 using std::string;
 using std::cout;
 using std::endl;
 using std::exception;
+using std::ostringstream;
 
 int stringToInt(string str);
 
 double stringToDouble(string str);
 char* stringToCharPointer(string& str, char* name);
+string doubleToString(double num);
