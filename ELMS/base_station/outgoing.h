7cee0eec437fa2bdd7dfd8fb768f9a41484ea9b6
kretzscd <kretzscd@oregonstate.edu>
2020-04-28 18:52:03 -0600

    Modified function declarations.

--- ELMS/base_station/outgoing.h
+++ ELMS/base_station/outgoing.h
@@ -3,7 +3,7 @@
 #include <string>
 using std::string;
 
-void outgoing_message(string& filePath, int unit1, int unit2, int messageTime, int velocity, int distance, int bearing);
+void outgoing_message(string &fileName, int unit1, int unit2, int messageTime, int velocity, int distance, int bearing);
 void network_failure_message(string& networkFailureMsg);
 void misc_error_message(string& miscErrorMsg);
 #endif
