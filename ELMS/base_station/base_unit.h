6fe0ab922dc92d66ee92c61932adfdfd64f5b6d8
kretzscd <kretzscd@oregonstate.edu>
2020-05-16 16:30:21 -0600

    Added default constructor.

--- ELMS/base_station/base_unit.h
+++ ELMS/base_station/base_unit.h
@@ -5,6 +5,7 @@
 #define BASE_UNIT_H
 
 #define MESSAGE_LIMIT 5
+#define SECONDS_LIMIT 10
 
 #include <iostream>
 #include <fstream>
@@ -53,14 +54,12 @@ class Base_Unit
     // to allow the user to create the folder wherever they want.  
 public:
 
-    // class destructor will manage memory leak by deleting pointings to
-    // vehicle objects in the mine_vehicles vector
-    ~Base_Unit();
-
     //Constructors
-    Base_Unit();
-    Base_Unit(FileIO *_f);
+    Base_Unit(FileIO* _f);
 
+    // class destructor will manage memory leak by deleting pointings to
+    // vehicle objects in the mine_vehicles vector.
+    ~Base_Unit();
 
     void logToFile(std::string, MessageType);
     void addToMineVehicles(Vehicle* v);
@@ -86,7 +85,8 @@ public:
     map<int, double> checkDistancesInMasterVector1(Vehicle* v);
     int checkOtherVehiclesPriorityNumbers(Vehicle* v1, int index, int priority_number);
 
-    //Base_Unit() : fileHandler("C:\\logs", MESSAGE_LIMIT) {}; // https://stackoverflow.com/questions/849812/c-construction-of-an-object-inside-a-class
+   // Base_Unit() : fileHandler("C:\\logs", MESSAGE_LIMIT) {}; // https://stackoverflow.com/questions/849812/c-construction-of-an-object-inside-a-class
+    Base_Unit() : fileHandler() {};
 };
 
 #endif // !BASE_UNIT_H
