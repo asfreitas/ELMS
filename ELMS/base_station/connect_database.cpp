82294c703b3dde80cae758f6e4e09dd252bf6d67
Tframe <tframe67@gmail.com>
2020-05-25 12:13:59 -0700

    Update conenct_database.cpp to change startup time based on status
    Update conenct_database.cpp to change startup time based on status

--- ELMS/base_station/connect_database.cpp
+++ ELMS/base_station/connect_database.cpp
@@ -12,16 +12,14 @@ Database::Database(std::string _uri)
 //desctructor
 Database::~Database()
 {
-    //std::cout << "This can't go out of scope until the"
-        //"program closes otherwise we lose the connection";
-    cout << "Final count for database connections is " << count << endl;
+    std::cout << "This can't go out of scope until the"
+        "program closes otherwise we lose the connection";
     delete pool;
 }
 
 //create a connection pool
 Database::connection Database::getConnection()
 {
-    count++;
     return pool->acquire();
 }
 
@@ -33,7 +31,6 @@ void Database::addVehicle(Vehicle* vehicle) {
     //get date-time using the zulu time in the vehicle
     time_t currentDateTime = zuluToDate(currentTime);
     milliseconds current_message_time = milliseconds(currentDateTime * 1000);
-
 
     try {
         //establish pool connection
@@ -54,6 +51,7 @@ void Database::addVehicle(Vehicle* vehicle) {
                 << "distance" << element.second
                 << close_document{};
         }
+		//fill in remaining data to be stored into mongodb
         auto after_array = sub_array << close_array;
         after_array
             << "vehicle_unit" << vehicle->getUnit()
@@ -74,19 +72,19 @@ void Database::addVehicle(Vehicle* vehicle) {
             << "new_bearing" << vehicle->getBearing()
             << "status" << vehicle->getStatus()
             << "priority" << vehicle->getPriorityNumber();
-        bsoncxx::document::value doc_view = after_array << finalize;
+            bsoncxx::document::value doc_view = after_array << finalize;
 
         //obtain view of document to insert it
         bsoncxx::document::view view = doc_view.view();
         //insert the view of the document
         bsoncxx::stdx::optional<mongocxx::result::insert_one> result = vehicles.insert_one(view);
         if (result) {
-            //std::cout << "Vehicle successfully created" << "\n";
+            std::cout << "Vehicle succuessfully created" << "\n";
         }
         else {
             std::cout << "Unsuccessful with creating" << "\n";
         }
-
+        
     }
     catch (mongocxx::exception& e)
     {
@@ -118,6 +116,7 @@ void Database::updateVehicle(Vehicle* vehicle)
         time_t lastDateTime = zuluToDate(lastTime);
         milliseconds last_message_time = milliseconds(lastDateTime * 1000);
 
+		//If vehicle status now offline, set startup_time to null
         if (vehicle->getStatus() == "offline") {
             bsoncxx::stdx::optional<mongocxx::result::update> update_startup =
                 vehicles.update_one(document{}
@@ -129,8 +128,10 @@ void Database::updateVehicle(Vehicle* vehicle)
                     << close_document{}
                     << finalize
                 );
+            vehicle->setStartupTime(NULL);
         }
-        else if (vehicle->getStatus() == "active") {
+        //else if status is not "offline" and the startup time is 0, reset startup time
+        else if(vehicle->getStartupTime() == NULL){
             bsoncxx::stdx::optional<mongocxx::result::update> update_startup =
                 vehicles.update_one(document{}
                     << "vehicle_unit" << vehicle->getUnit()
@@ -141,6 +142,7 @@ void Database::updateVehicle(Vehicle* vehicle)
                     << close_document{}
                     << finalize
                 );
+            vehicle->setStartupTime(vehicle->getTime());
         }
 
         //get a map of distance to vehicles vector
@@ -148,7 +150,7 @@ void Database::updateVehicle(Vehicle* vehicle)
 
         bsoncxx::builder::stream::document builder{};
         //first, build distance_to_vehicles array with objects of other vehicles
-        auto sub_array = builder << "$set" << open_document{}
+        auto sub_array = builder << "$set" << open_document{} 
         << "distance_to_vehicles" << open_array;
         for (pair<int, double> element : *mapVehicles) {
             sub_array = sub_array << open_document{}
@@ -156,10 +158,10 @@ void Database::updateVehicle(Vehicle* vehicle)
                 << "distance" << element.second
                 << close_document{};
         }
+		//fill in remaining data to be set into mongodb
         auto after_array = sub_array << close_array;
         after_array
             << "vehicle_unit" << vehicle->getUnit()
-            //<< "startup_time" << bsoncxx::types::b_date{ message_time }
             << "last_received_time" << bsoncxx::types::b_date{ last_message_time }
             << "last_longitude" << vehicle->getPreviousLongitude()
             << "last_latitude" << vehicle->getPreviousLatitude()
@@ -172,6 +174,7 @@ void Database::updateVehicle(Vehicle* vehicle)
             << "priority" << vehicle->getPriorityNumber();
         bsoncxx::document::value doc_view = after_array << close_document{} << finalize;
         bsoncxx::document::view view = doc_view.view();
+		//update the vehicle
         auto result = vehicles.update_one(
             document{} << "vehicle_unit" << vehicle->getUnit() << finalize,
             view);
@@ -183,15 +186,15 @@ void Database::updateVehicle(Vehicle* vehicle)
         {
             std::cout << "There was a problem updating\n";
         }
-
+		//push data onto the arrays
         bsoncxx::stdx::optional<mongocxx::result::update> update_array =
             vehicles.update_one(document{}
                 << "vehicle_unit" << vehicle->getUnit()
                 << finalize,
                 document{}
                 << "$push" << open_document{}
-                << "past_velocity" << vehicle->getVelocity()
-                << "past_bearing" << vehicle->getBearing()
+                    << "past_velocity" << vehicle->getVelocity()
+                    << "past_bearing" << vehicle->getBearing()
                 << close_document{}
                 << finalize
             );
