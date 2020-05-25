78aa678113e61900b2cec904d0ab7133a133ef86
kretzscd <kretzscd@oregonstate.edu>
2020-05-25 17:54:42 -0600

    Added CreateAlert function definiton

--- ELMS/base_station/outgoing.h
+++ ELMS/base_station/outgoing.h
@@ -2,11 +2,13 @@
 #define OUTGOING_H
 
 #include "utilities.h"
-
+#include "calculations.h"
 #include <string>
 using std::string;
 
 void outgoing_message(string& alertMessage, int unit1, int unit2, int messageTime, double velocity, double distance, double bearing);
+void createAlert(string& alertMessage, string& alertLogMessage, char* fileName,
+    double velocity, Vehicle* v1, Vehicle* v2, double distance, int time);
 void network_failure_message(string& networkFailureMsg);
 void misc_error_message(string& miscErrorMsg);
 #endif
